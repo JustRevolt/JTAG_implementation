f707f1c8ff2cbb173c3c703794484911c7ebdb35
JustRevolt <38432080+JustRevolt@users.noreply.github.com>
2022-12-18 18:26:48 +0300

    other test change after DUT change

--- JTAG.srcs/sim_1/new/tb.sv
+++ JTAG.srcs/sim_1/new/tb.sv
@@ -9,10 +9,10 @@ logic device_id_tb_res;
 logic bypass_tb_res;
 
 tap_control_tb tap_controller(.result(tap_control_tb_res));
-//instruct_reg_tb instruct_reg(.result(instruct_reg_tb_res)); 
-//inst_decode_tb decode(.result(inst_decode_tb_res));
-//device_id_tb device_id(.result(device_id_tb_res));
-//bypass_tb bypass(.result(bypass_tb_res));
+instruct_reg_tb instruct_reg(.result(instruct_reg_tb_res)); 
+inst_decode_tb decode(.result(inst_decode_tb_res));
+device_id_tb device_id(.result(device_id_tb_res));
+bypass_tb bypass(.result(bypass_tb_res));
 
 
 endmodule
