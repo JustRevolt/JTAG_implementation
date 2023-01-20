770809e8692d8d6529759b0f67f68b3b7d9f9b35
JustRevolt <38432080+JustRevolt@users.noreply.github.com>
2023-01-20 17:07:43 +0300

    test without TAP

--- JTAG.srcs/sources_1/new/TAP.sv
+++ JTAG.srcs/sources_1/new/TAP.sv
@@ -14,94 +14,103 @@ module TAP
     output logic out_BSC_o [OUT_BSC_COUNT-1:0]
     );
     
-    localparam INSRUCT_LENGTH = 3;
+//    localparam INSRUCT_LENGTH = 3;
     
-    logic inTest_mode, exTest_mode, bypass_mode, device_id_mode, system_mode;
+//    logic inTest_mode, exTest_mode, bypass_mode, device_id_mode, system_mode;
 
-    logic shiftDR, captureDR, updDR, outDR, normal_mode;
-    logic shiftIR, captureIR, updIR, outIR;
+//    logic shiftDR, captureDR, updDR, outDR, normal_mode;
+//    logic shiftIR, captureIR, updIR, outIR;
 
-    logic [INSRUCT_LENGTH-1:0] instruction;
+//    logic [INSRUCT_LENGTH-1:0] instruction;
 
-    logic reg_select, out_mux_o, out_reg, tck, enable, rst;
+//    logic reg_select, out_mux_o, out_reg, tck, enable, rst;
     
-    assign normal_mode = system_mode | rst;
+//    assign normal_mode = system_mode | rst;
 
-    test_data_regs #(.IN_BSC_COUNT(IN_BSC_COUNT), 
-                        .OUT_BSC_COUNT(OUT_BSC_COUNT)) DR 
-    (
-        .tck_i(tck),
-        .shift_i(shiftDR),
-        .capture_i(captureDR),
-        .upd_i(updDR),
-        .inTest_i(inTest_mode),
-        .exTest_i(exTest_mode),
-        .normal_mode_i(normal_mode),
-        .mux_g0_i(bypass_mode),
-        .mux_g1_i(device_id_mode),
-        .data_i(TDI_i),
-        .in_BSC_i(in_BSC_i),
-        .out_BSC_i(out_BSC_i),
-        .data_o(outDR),
-        .in_BSC_o(in_BSC_o),
-        .out_BSC_o(out_BSC_o)
-    );
+//    test_data_regs #(.IN_BSC_COUNT(IN_BSC_COUNT), 
+//                        .OUT_BSC_COUNT(OUT_BSC_COUNT)) DR 
+//    (
+//        .tck_i(tck),
+//        .shift_i(shiftDR),
+//        .capture_i(captureDR),
+//        .upd_i(updDR),
+//        .inTest_i(inTest_mode),
+//        .exTest_i(exTest_mode),
+//        .normal_mode_i(normal_mode),
+//        .mux_g0_i(bypass_mode),
+//        .mux_g1_i(device_id_mode),
+//        .data_i(TDI_i),
+//        .in_BSC_i(in_BSC_i),
+//        .out_BSC_i(out_BSC_i),
+//        .data_o(outDR),
+//        .in_BSC_o(in_BSC_o),
+//        .out_BSC_o(out_BSC_o)
+//    );
 
-    instruction_regs 
-    #(  .REG_LENGTH(INSRUCT_LENGTH), 
-        .SPEC_DATA(3'b101),
-        .DEFAULT_INSTRUCT(3'b011)) 
-    IR (
-        .rst_i(rst),
-        .tck_i(tck),
-        .shift_i(shiftIR),
-        .capture_i(captureIR),
-        .upd_i(updIR),
-        .data_i(TDI_i),
-        .data_o(outIR),
-        .instruction_o(instruction)
-    );
+//    instruction_regs 
+//    #(  .REG_LENGTH(INSRUCT_LENGTH), 
+//        .SPEC_DATA(3'b101),
+//        .DEFAULT_INSTRUCT(3'b011)) 
+//    IR (
+//        .rst_i(rst),
+//        .tck_i(tck),
+//        .shift_i(shiftIR),
+//        .capture_i(captureIR),
+//        .upd_i(updIR),
+//        .data_i(TDI_i),
+//        .data_o(outIR),
+//        .instruction_o(instruction)
+//    );
 
-    mux_2to1 out_mux (
-        .in0(outDR), 
-        .in1(outIR),
-        .g(reg_select),
-        .out(out_mux_o)
-    );
+//    mux_2to1 out_mux (
+//        .in0(outDR), 
+//        .in1(outIR),
+//        .g(reg_select),
+//        .out(out_mux_o)
+//    );
 
-    instruction_decoder #(.INSTR_LENGTH(INSRUCT_LENGTH)) inst_decoder(
-        .instruction_i(instruction),
-        .inTest_o(inTest_mode),
-        .exTest_o(exTest_mode),
-        .system_mode_o(system_mode),
-        .bypass_o(bypass_mode),
-        .device_id_o(device_id_mode)
-    );
+//    instruction_decoder #(.INSTR_LENGTH(INSRUCT_LENGTH)) inst_decoder(
+//        .instruction_i(instruction),
+//        .inTest_o(inTest_mode),
+//        .exTest_o(exTest_mode),
+//        .system_mode_o(system_mode),
+//        .bypass_o(bypass_mode),
+//        .device_id_o(device_id_mode)
+//    );
 
-    TAP_controller tap_control(
-        .tck_i(TCK_i),
-        .tms_i(TMS_i),
+//    TAP_controller tap_control(
+//        .tck_i(TCK_i),
+//        .tms_i(TMS_i),
         
-        .shiftDR_o(shiftDR),
-        .captureDR_o(captureDR),
-        .updDR_o(updDR),
+//        .shiftDR_o(shiftDR),
+//        .captureDR_o(captureDR),
+//        .updDR_o(updDR),
         
-        .shiftIR_o(shiftIR),
-        .captureIR_o(captureIR),
-        .updIR_o(updIR),
+//        .shiftIR_o(shiftIR),
+//        .captureIR_o(captureIR),
+//        .updIR_o(updIR),
         
-        .reg_select_o(reg_select),
-        .tck_o(tck),
-        .rst_o(rst),
-        .enable_o(enable)
-    );
-//      assign TDO_o = 0;  
-    assign TDO_o = enable ? out_reg : 1'bZ;
-//    assign TDO_o = out_reg;
+//        .reg_select_o(reg_select),
+//        .tck_o(tck),
+//        .rst_o(rst),
+//        .enable_o(enable)
+//    );
+////      assign TDO_o = 0;  
+//    assign TDO_o = enable ? out_reg : 1'bZ;
+////    assign TDO_o = out_reg;
       
-//TODO Check the slack
-    always @ (negedge tck) begin
-        out_reg <= out_mux_o;
+////TODO Check the slack
+//    always @ (negedge tck) begin
+//        out_reg <= out_mux_o;
+//    end
+    
+    logic out_reg = 0, tms_reg = 0;
+    
+    assign TDO_o = out_reg;
+    
+    always @ (posedge TCK_i) begin
+        tms_reg <= TMS_i;
+        out_reg <= TDI_i;
     end
-
+    
 endmodule
--- JTAG.srcs/sources_1/new/top.sv
+++ JTAG.srcs/sources_1/new/top.sv
@@ -8,95 +8,35 @@ module top(
     input logic tap_tck_i,          // JA_4 = G17
     input logic tap_tms_i,          // JA_3 = E18
     input logic tap_tdi_i,          // JA_2 = D18
-    output logic tap_tdo_o,         // JA_1 = C17
-    
-    input logic [2:0] data_pass_i,  // sw_2:0 = M13 L16 J15
-    input logic test_reg_i         // btn_l = P17
+    output logic tap_tdo_o         // JA_1 = C17
      
-//    ,output logic anlz_dec_clk_o    // JC_1         = K1
-//    ,output logic anlz_dec_rst_o    // JC_2         = F6
+    ,output logic anlz_dec_clk_o    // JC_1         = K1
+    ,output logic anlz_dec_rst_o    // JC_2         = F6
 //    ,output logic [2:0] anlz_dec_data_o // JC_9:7   = G13, F13, E16
     
-//    ,output logic anlz_tap_tck_o    // JD_1 = H4
-//    ,output logic anlz_tap_tms_o    // JD_2 = H1
-//    ,output logic anlz_tap_tdi_o    // JD_3 = G1
-//    ,output logic anlz_tap_tdo_o     // JD_4 = G3
+    ,output logic anlz_tap_tck_o    // JD_1 = H4
+    ,output logic anlz_tap_tms_o    // JD_2 = H1
+    ,output logic anlz_tap_tdi_o    // JD_3 = G1
+    ,output logic anlz_tap_tdo_o     // JD_4 = G3
     );
     
-    localparam IN_BSC_COUNT = 3;
-    localparam OUT_BSC_COUNT = 3;
-    
-    logic dec_clk;
-    logic dec_rst;
-    logic [2:0] dec_data;
-    logic test_reg;
-    
-    logic in_BSC_input [IN_BSC_COUNT-1:0];
-    logic in_BSC_output [IN_BSC_COUNT-1:0];
-    
-    logic out_BSC_input [OUT_BSC_COUNT-1:0];
-    logic out_BSC_output [OUT_BSC_COUNT-1:0];
-    
-    assign dec_rst = in_BSC_output[2];
-    assign dec_clk = in_BSC_output[1];
-    
-    always @ (posedge tap_tck_i) 
-        test_reg <= in_BSC_output[0];
-    
-    assign out_BSC_input = {dec_data[2], dec_data[1], dec_data[0]};
-    
-    assign dec_data_o[2] = out_BSC_output[2];
-    assign dec_data_o[1] = out_BSC_output[1];
-    assign dec_data_o[0] = out_BSC_output[0];
-    
-//    assign anlz_dec_clk_o = dec_clk_i;
-//    assign anlz_dec_rst_o = dec_rst_i;
+    assign anlz_dec_clk_o = dec_clk_i;
+    assign anlz_dec_rst_o = dec_rst_i;
 //    assign anlz_dec_data_o = dec_data_o;
     
-//    assign anlz_tap_tck_o = tap_tck_i;
-//    assign anlz_tap_tms_o = tap_tms_i;
-//    assign anlz_tap_tdi_o = tap_tdi_i;
-//    assign anlz_tap_tdo_o = tap_tdo_o;
-    
-    mux_2to1 dec_rst_mux (
-        .in0(dec_data_o[0]), 
-        .in1(dec_rst_i),
-        .g(data_pass_i[2]),
-        .out(in_BSC_input[2])
-    );
-    
-    mux_2to1 dec_clk_mux (
-        .in0(dec_data_o[1]), 
-        .in1(dec_clk_i),
-        .g(data_pass_i[1]),
-        .out(in_BSC_input[1])
-    );
-    
-    mux_2to1 test_reg_mux (
-        .in0(dec_data_o[2]), 
-        .in1(test_reg_i),
-        .g(data_pass_i[0]),
-        .out(in_BSC_input[0])
-    );
+    assign anlz_tap_tck_o = tap_tck_i;
+    assign anlz_tap_tms_o = tap_tms_i;
+    assign anlz_tap_tdi_o = tap_tdi_i;
+    assign anlz_tap_tdo_o = tap_tdo_o;
     
     dec_counter dec(
-        .clk_i(dec_clk),
-        .rst_i(dec_rst),
-        .data_o(dec_data)
+        .clk_i(dec_clk_i),
+        .rst_i(dec_rst_i),
+        .data_o(dec_data_o)
     );
-
-    TAP
-    #(  .IN_BSC_COUNT(IN_BSC_COUNT), 
-        .OUT_BSC_COUNT(OUT_BSC_COUNT)) 
-    tap(
-        .TCK_i(tap_tck_i),
-        .TMS_i(tap_tms_i),
-        .TDI_i(tap_tdi_i),
-        .in_BSC_i(in_BSC_input),
-        .out_BSC_i(out_BSC_input),
-        .TDO_o(tap_tdo_o),
-        .in_BSC_o(in_BSC_output),
-        .out_BSC_o(out_BSC_output)
-    );
-
+    
+    always @ (posedge tap_tck_i) begin
+        tap_tdo_o <= tap_tdi_i;
+    end
+    
 endmodule
