05ffaf0897c6b1055fd26f4a72a6c006058b14b7
JustRevolt <38432080+JustRevolt@users.noreply.github.com>
2023-01-23 15:55:55 +0300

    top without ExTest & dec_rst = 1

--- JTAG.srcs/sources_1/new/dec_counter.sv
+++ JTAG.srcs/sources_1/new/dec_counter.sv
@@ -6,8 +6,8 @@ module dec_counter(
     output logic [2:0] data_o
     );
 
-    always @ (posedge clk_i or negedge rst_i) begin
-        if (!rst_i) data_o <= 3'b111;
+    always @ (posedge clk_i or posedge rst_i) begin
+        if (rst_i) data_o <= 3'b111;
         else data_o <= data_o - 1;
     end
    
--- JTAG.srcs/sources_1/new/top.sv
+++ JTAG.srcs/sources_1/new/top.sv
@@ -9,34 +9,40 @@ module top(
     input logic tap_tms_i,          // JA_3 = E18
     input logic tap_tdi_i,          // JA_2 = D18
     output logic tap_tdo_o         // JA_1 = C17
-     
-    ,output logic anlz_dec_clk_o    // JC_1         = K1
-    ,output logic anlz_dec_rst_o    // JC_2         = F6
-//    ,output logic [2:0] anlz_dec_data_o // JC_9:7   = G13, F13, E16
-    
-    ,output logic anlz_tap_tck_o    // JD_1 = H4
-    ,output logic anlz_tap_tms_o    // JD_2 = H1
-    ,output logic anlz_tap_tdi_o    // JD_3 = G1
-    ,output logic anlz_tap_tdo_o     // JD_4 = G3
     );
     
-    assign anlz_dec_clk_o = dec_clk_i;
-    assign anlz_dec_rst_o = dec_rst_i;
-//    assign anlz_dec_data_o = dec_data_o;
+    localparam IN_BSC = 2;
+    localparam OUT_BSC = 3;
     
-    assign anlz_tap_tck_o = tap_tck_i;
-    assign anlz_tap_tms_o = tap_tms_i;
-    assign anlz_tap_tdi_o = tap_tdi_i;
-    assign anlz_tap_tdo_o = tap_tdo_o;
+    logic dec_clk, dec_rst;
+    logic [2:0] dec_data;
+    
+    logic in_BSC_output [IN_BSC-1:0];
+    logic out_BSC_output [OUT_BSC-1:0];
+    
+    assign dec_clk = in_BSC_output[0];
+    assign dec_rst = in_BSC_output[1];
+    
+    assign {>>{dec_data_o}} = out_BSC_output;
     
     dec_counter dec(
-        .clk_i(dec_clk_i),
-        .rst_i(dec_rst_i),
-        .data_o(dec_data_o)
+        .clk_i(dec_clk),
+        .rst_i(dec_rst),
+        .data_o(dec_data)
     );
     
-    always @ (posedge tap_tck_i) begin
-        tap_tdo_o <= tap_tdi_i;
-    end
-    
+    //TAP
+    TAP #(
+        .IN_BSC_COUNT(IN_BSC), 
+        .OUT_BSC_COUNT(OUT_BSC))
+    tap(
+        .TCK_i(tap_tck_i)
+      , .TMS_i(tap_tms_i)
+      , .TDI_i(tap_tdi_i)
+      , .in_BSC_i({~dec_rst_i, dec_clk_i})
+      , .out_BSC_i({dec_data[2],dec_data[1],dec_data[0]})
+      , .TDO_o(tap_tdo_o)
+      , .in_BSC_o(in_BSC_output)
+      , .out_BSC_o(out_BSC_output)
+    );
 endmodule
